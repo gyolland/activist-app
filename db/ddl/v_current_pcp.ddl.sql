bc9636d75564e0ce6aa26af8cc187f874f3039f5
George Yolland <gyolland@yahoo.com>
2018-10-02 21:28:34 -0700

    modified v_current_pcp view, and added previously missed t_person_role.ddl.sql

--- db/ddl/v_current_pcp.ddl.sql
+++ db/ddl/v_current_pcp.ddl.sql
@@ -1,6 +1,8 @@
--CREATE VIEW `v_current_pcp` AS
+DROP VIEW IF EXISTS v_current_pcp;
+CREATE VIEW `v_current_pcp` AS
 SELECT
     p.vanid AS VANID
+  , p.vanprecinct AS VANPrecinct
   , p.precinct AS 'MCEDPrecinct'
   , p.cd AS 'CD'
   , p.lname AS 'LastName'
@@ -16,21 +18,25 @@ SELECT
   , ar.zip4 AS 'Zip4'
   , 'US' AS 'CountryCode'
   , p.primary_email AS 'PreferredEmail'
-  , p.primary_phone AS 'Home Phone'
-  , '' AS 'Cell Phone'
+  , p.primary_phone AS 'Home/Phone 1'
+  , p.allow_text1 AS 'Phone 1 Allow Text'
+  , p.phone2 AS 'Cell/Phone 2'
+  , p.allow_text2 AS 'Phone 2 Allow Text'
   , am.address AS 'MailingAddress'
   , am.city AS 'MailingCity'
   , am.state AS 'MailingState'
   , CASE WHEN am.zip4 IS NULL
-	THEN am.zip5
+	  THEN am.zip5
     ELSE concat(am.zip5, '-', am.zip4)
     END AS 'MailingZip'
-  , replace(c.comment, 'assignment: ', '') AS 'ASSIGNMENT'
+  , CASE WHEN r0.elected = TRUE THEN 'Elected' ELSE 'Appointed' END AS 'Elected or Appointed'
+  , CASE WHEN p.nda_on_file = TRUE THEN 'Yes' ELSE 'No' END AS 'NDA on File'
+  , p.assignment AS 'ASSIGNMENT'
 FROM t_person p 
-JOIN t_person_role pr ON p.id = pr.person_id
+JOIN t_person_role r0 ON p.id = r0.person_id
 JOIN (SELECT * FROM t_address WHERE type = 'RESI') ar ON p.id = ar.person_id
 LEFT JOIN (SELECT * FROM t_address WHERE type = 'MAIL') am ON p.id = am.person_id
-LEFT JOIN (SELECT obj_id, comment FROM t_comment WHERE comment LIKE '%assignment:%') c ON p.id = c.obj_id
-WHERE pr.role_id = 3
-AND pr.term_start_date < now()
-AND pr.term_end_date > now();
+LEFT JOIN t_comment c ON p.id = c.obj_id
+WHERE r0.role_id = 3
+AND r0.term_start_date < now()
+AND r0.term_end_date > now();
