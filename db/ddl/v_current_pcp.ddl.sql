938a7d62368ced0235de55b62435931665776e94
George Yolland <gyolland@yahoo.com>
2018-10-04 21:46:17 -0700

    added suffix to t_person, renamed v_current_pcp view columns to make this selectable

--- db/ddl/v_current_pcp.ddl.sql
+++ db/ddl/v_current_pcp.ddl.sql
@@ -1,36 +1,36 @@
 CREATE OR REPLACE VIEW `v_current_pcp` AS
 SELECT
-    p.vanid AS VANID
-  , p.vanprecinct AS VANPrecinct
-  , p.precinct AS 'MCEDPrecinct'
-  , p.cd AS 'CD'
-  , p.lname AS 'LastName'
-  , p.fname AS 'FirstName'
-  , p.middlename AS 'MiddleName'
-  , p.nickname AS 'NickName'
-  , NULL AS 'Suffix'
-  , p.gender AS 'Sex'
-  , ar.address AS 'Address'
-  , ar.city AS 'City'
-  , ar.state AS 'State/Province'
-  , ar.zip5 AS 'Zip/Postal'
-  , ar.zip4 AS 'Zip4'
-  , 'US' AS 'CountryCode'
-  , p.primary_email AS 'PreferredEmail'
-  , p.primary_phone AS 'Home/Phone 1'
-  , CASE WHEN p.allow_text1 = TRUE THEN 'Yes' ELSE 'No' END AS 'Phone 1 Allow Text'
-  , p.phone2 AS 'Cell/Phone 2'
-  , CASE WHEN p.allow_text2 = TRUE THEN 'Yes' ELSE 'No' END AS 'Phone 2 Allow Text'
-  , am.address AS 'MailingAddress'
-  , am.city AS 'MailingCity'
-  , am.state AS 'MailingState'
+    p.vanid
+  , p.vanprecinct
+  , p.precinct AS mcedprecinct
+  , p.cd 
+  , p.lname AS LastName
+  , p.fname AS FirstName
+  , p.middlename AS MiddleName
+  , p.nickname AS NickName
+  , suffix
+  , p.gender AS gender
+  , ar.address
+  , ar.city
+  , ar.state
+  , ar.zip5
+  , ar.zip4
+  , 'US' AS countrycode
+  , p.primary_email AS preferredemail
+  , p.primary_phone AS home_phone1
+  , CASE WHEN p.allow_text1 = TRUE THEN 'Yes' ELSE 'No' END AS phone1_allow_text
+  , p.phone2 AS cell_phone2
+  , CASE WHEN p.allow_text2 = TRUE THEN 'Yes' ELSE 'No' END AS phone2_allow_text
+  , am.address AS mailingaddress
+  , am.city AS mailingcity
+  , am.state AS mailingstate
   , CASE WHEN am.zip4 IS NULL
 	  THEN am.zip5
     ELSE concat(am.zip5, '-', am.zip4)
-    END AS 'MailingZip'
-  , CASE WHEN r0.elected = TRUE THEN 'Elected' ELSE 'Appointed' END AS 'Elected or Appointed'
-  , CASE WHEN p.nda_on_file = TRUE THEN 'Yes' ELSE 'No' END AS 'NDA on File'
-  , p.assignment AS 'ASSIGNMENT'
+    END AS mailingzip
+  , CASE WHEN r0.elected = TRUE THEN 'Elected' ELSE 'Appointed' END AS elected_or_appointed
+  , CASE WHEN p.nda_on_file = TRUE THEN 'Yes' ELSE 'No' END AS nda_on_file
+  , p.assignment
 FROM t_person p 
 JOIN t_person_role r0 ON p.id = r0.person_id
 JOIN (SELECT * FROM t_address WHERE type = 'RESI') ar ON p.id = ar.person_id
