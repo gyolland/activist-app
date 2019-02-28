ec731a645ae4119796344af6d10e6d6526c903f2
George Yolland <gyolland@yahoo.com>
2019-02-27 22:13:39 -0800

    modified t_person_role table to have an auto incremented key independent of foreign key references

--- db/ddl/t_person_role.ddl.sql
+++ db/ddl/t_person_role.ddl.sql
@@ -1,4 +1,5 @@
 CREATE TABLE `t_person_role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `person_id` int(11) NOT NULL,
   `role_id` int(11) NOT NULL,
   `certified_pcp` tinyint(1) DEFAULT NULL,
@@ -6,8 +7,8 @@ CREATE TABLE `t_person_role` (
   `term_start_date` date DEFAULT NULL,
   `term_end_date` date DEFAULT NULL,
   `inactive` tinyint(1) DEFAULT NULL,
-  PRIMARY KEY (`person_id`,`role_id`),
+  PRIMARY KEY (`id`),
   KEY `role_id` (`role_id`),
   CONSTRAINT `FK_PERSON` FOREIGN KEY (`person_id`) REFERENCES `t_person` (`id`) ON DELETE CASCADE,
   CONSTRAINT `FK_ROLE` FOREIGN KEY (`role_id`) REFERENCES `t_role` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ;
