4b287798568d0a0bbf6bc31364c97ad115e403a2
George Yolland <gyolland@yahoo.com>
2019-05-01 10:07:20 -0700

    fixed bug when requesting n+1, when on n is availble should return null

--- db/proc/f_get_word_part.ddl.sql
+++ db/proc/f_get_word_part.ddl.sql
@@ -1,4 +1,4 @@
-DROP FUNCTION IF EXISTS `f_get_word_part`;
+DROP FUNCTION IF EXISTS f_get_word_part;
 
 DELIMITER $$
 /********************************************************************************
@@ -9,7 +9,7 @@ separate by spaces. For practicality, the maximun number of words supported will
 be limited to 10 words.
 ********************************************************************************/
 
-CREATE FUNCTION `f_get_word_part` (p_word_str VARCHAR(100), p_part_no INT)
+CREATE FUNCTION f_get_word_part (p_word_str VARCHAR(100), p_part_no INT)
 RETURNS VARCHAR(40)
 BEGIN
     DECLARE l_word_part VARCHAR(40);
@@ -19,22 +19,30 @@ BEGIN
 
     -- trim leading spaces and
     -- ensure there is at least 1 space on the end
-    SET l_string = CONCAT(LTRIM(p_word_str), ' ');
+    SET l_string = TRIM(p_word_str);
 
-    WHILE Xi < p_part_no DO 
-        SET l_pos = INSTR(l_string, ' ');
+    -- eliminate multiple continuous spaces
+    WHILE INSTR(l_string, '  ') > 0  DO
+        SET l_string = REPLACE(l_string, '  ', ' ');
+    END WHILE;
+
+    WHILE INSTR(l_string, ' ') > 0 DO
+        SET l_pos = INSTR(l_string, ' ');  -- 'John Jakob Jingle Jimer'
         SET l_word_part = TRIM(LEFT(l_string, l_pos));
-        SET l_string = LTRIM(SUBSTR(l_string, l_pos, 100));
+        SET l_string = CONCAT(TRIM(SUBSTR(l_string, l_pos, 100)), ' ');
 
-        IF LENGTH(TRIM(l_string)) > 0 THEN
-            SET Xi = Xi + 1;
-        ELSE
-            SET Xi = p_part_no;
+        SET Xi = Xi + 1;
+        IF Xi >= p_part_no THEN
+            SET l_string = '';
         END IF;
     END WHILE;
 
     SET l_word_part = REPLACE(l_word_part, ',', '');
     SET l_word_part = REPLACE(l_word_part, '.', '');
+
+    IF NOT LENGTH(l_word_part) > 0 THEN
+        SET l_word_part = NULL;
+    END IF;
 
 RETURN l_word_part;
 END $$
