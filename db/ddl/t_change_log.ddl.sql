17f113f1024441a08baf3fcb41e755408044626d
George Yolland <gyolland@yahoo.com>
2019-05-08 22:31:18 -0700

    added person_role_id to provide a better trail back to record that was modified

--- db/ddl/t_change_log.ddl.sql
+++ db/ddl/t_change_log.ddl.sql
@@ -5,8 +5,8 @@ CREATE TABLE IF NOT EXISTS t_change_log (
     id INT(11) NOT NULL AUTO_INCREMENT,
     changed TIMESTAMP DEFAULT current_timestamp(),
     action VARCHAR(20) DEFAULT null,
-    person_id INT(11) NOT NULL,
+    person_id INT(11),
+    person_role_id INT(11),
     logmessage VARCHAR(1000),
     PRIMARY KEY(id) 
 );
-
