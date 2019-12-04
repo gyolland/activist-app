aeac08e0ce400d85cef00178bf4f9ac85d41f95d
George Yolland <gyolland@yahoo.com>
2019-12-03 20:53:53 -0800

    added assignment, vanid, reordered columns

--- db/sqlscripts/id_new_pcp.sql
+++ db/sqlscripts/id_new_pcp.sql
@@ -1,7 +1,7 @@
 --  * new PCPs, 
 -- select new ocvr entries, missing from t_person
 SELECT
-     'NEW' AS status
+     p.vanid
   ,  p.id AS person_id
   ,  o.precinct
   ,  o.ocvr_voter_id
@@ -12,9 +12,11 @@ SELECT
      ELSE trim(substr(trim(o.fname), instr(trim(o.fname), ' '), 100))
      END middlename
   ,  o.lname
-  ,  o.gender
+  -- ,  o.gender
   ,  o.r_address
   ,  o.r_zip
+  ,  o.assignment
+  ,  'NEW' AS status
 FROM t_person p
 RIGHT JOIN t_import_ocvr_tmp o USING(ocvr_voter_id)
 WHERE p.id IS NULL;
