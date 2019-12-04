6a011b09ce36e12f5dd0b40bbef1361441607ff0
George Yolland <gyolland@yahoo.com>
2019-12-03 20:55:58 -0800

    added vanid, assignment, reordered columns. removed gender from the where clause

--- db/sqlscripts/id_removed_from_ocvr.sql
+++ db/sqlscripts/id_removed_from_ocvr.sql
@@ -3,7 +3,7 @@
 -- to prevent person identifying as non-binary gender appearing in this selection,
 -- gender <> 'X' is specified in the query. 
 SELECT
-     'REMOVED' AS status
+    vanid
   ,  p.id AS person_id
   ,  p.precinct
   ,  p.ocvr_voter_id
@@ -11,14 +11,15 @@ SELECT
   ,  p.fname
   ,  p.middlename
   ,  p.lname
-  ,  p.gender
+  -- ,  p.gender
   ,  a.residential_address AS r_address
   ,  a.residential_zip AS r_zip
+  ,  p.assignment
+  , 'REMOVED' AS status
 FROM t_person p
 JOIN t_person_role r0 ON p.id = r0.person_id
 JOIN v_address a ON p.id = a.person_id
 LEFT JOIN t_import_ocvr_tmp o USING(ocvr_voter_id)
-WHERE p.gender <> 'X'
-  AND r0.role_id = 3
+WHERE r0.role_id = 3
   AND r0.inactive = FALSE
   AND o.ocvr_voter_id IS NULL;
