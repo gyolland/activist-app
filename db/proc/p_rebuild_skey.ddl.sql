9d8b01de73691c00616058aa7ab39dc09fcc2fcc
George Yolland <gyolland@yahoo.com>
2019-12-17 21:16:54 -0800

    moving sqlscript into a procedure. Works but messaging needs improvement.

---
+++ db/proc/p_rebuild_skey.ddl.sql
@@ -0,0 +1,28 @@
+CREATE PROCEDURE p_rebuild_skey()
+BEGIN
+
+TRUNCATE t_skey;
+CREATE PROCEDURE p_rebuild_skey()
+BEGIN
+    -- rebuild_skey: truncates and repopulates a surrogate key table
+    TRUNCATE t_skey;
+
+    INSERT INTO t_skey (person_id, fname, middlename, lname, skey_long, skey_short)
+    SELECT
+        p.id AS person_id
+    , p.fname
+    , p.middlename
+    , p.lname
+    , f_skey(ifnull(e.d_fname, p.fname), ifnull(e.d_middlename, p.middlename), ifnull(e.d_lname, p.lname)) as skey_long
+    , f_skey(ifnull(e.d_fname, p.fname), '',  ifnull(e.d_lname, p.lname)) as skey_short
+    FROM t_person p
+    LEFT JOIN exception e ON p.id = e.person_id
+    ORDER BY p.id ;
+
+    SELECT skey_short, count(*) recs
+    FROM t_skey
+    GROUP BY skey_short
+    HAVING count(*) > 1;
+END //
+
+DELIMITER ;
