b35a2e49552200c607dcf766ec61d00d8466e307
George Yolland <gyolland@yahoo.com>
2019-12-17 21:20:00 -0800

    added stg_id column and increase category to varchar(100).

--- db/ddl/change_report.ddl.sql
+++ db/ddl/change_report.ddl.sql
@@ -1,8 +1,9 @@
 CREATE TABLE change_report (
     id              INT(11) AUTO_INCREMENT
+  , stg_id          INT(11) DEFAULT NULL
   , report_date     DATE DEFAULT NULL
   , person_id       INT(11) DEFAULT NULL
-  , category        VARCHAR(40) DEFAULT NULL
+  , category        VARCHAR(100) DEFAULT NULL
   , message         TEXT DEFAULT NULL
   , PRIMARY KEY rpt_pk_idx (id)
   , KEY person_idx (person_id)
