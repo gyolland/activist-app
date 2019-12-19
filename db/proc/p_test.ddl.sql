97190cfda6673d1ad36e652aab4de4ec859fb31d
George Yolland <gyolland@yahoo.com>
2019-12-18 20:53:03 -0800

    scraps that I want to save for now

--- db/proc/p_test.ddl.sql
+++ db/proc/p_test.ddl.sql
@@ -2,31 +2,54 @@ DROP PROCEDURE IF EXISTS p_test;
 
 DELIMITER //
 
-CREATE PROCEDURE p_test (IN p_state_voter_id VARCHAR(14), OUT person_id INT, inactive BOOLEAN)
+CREATE PROCEDURE p_test (IN p_person_id INT, IN p_report_date DATE, OUT o_error BOOLEAN, OUT o_message TEXT)
 BEGIN
     DECLARE done INT DEFAULT FALSE;
     DECLARE onError INT DEFAULT FALSE;
 
-    DECLARE v_person_id INT DEFAULT NULL;
-    DECLARE v_inactive BOOLEAN DEFAULT FALSE;
+    DECLARE v_person_id         INT         DEFAULT NULL;
+    DECLARE v_inactive          BOOLEAN     DEFAULT FALSE;
+    DECLARE v_person_role_id    INT         DEFAULT NULL;
 
-    prepare test_prep from 
-    'select p.id as person_id, r0.inactive INTO ?, ?                                                    
-    from t_person p                                                                                                              
-    left join (select person_id, inactive, max(term_end_date) term_end_date                                                      
-    from t_person_role                                                                                                           
-    where role_id = 3                                                                                                            
-    group by person_id, inactive ) r0 ON p.id = r0.person_id Where ocvr_voter_id = ?';
+    DECLARE c_pcp_role_id       INT         DEFAULT 3;
+    DECLARE c_inactive_true     BOOLEAN     DEFAULT TRUE;
+    DECLARE c_inactive_false    BOOLEAN     DEFAULT FALSE;
 
-    SET @vid = p_state_voter_id;
+    DECLARE CONTINUE HANDLER FOR SQLEXCEPTION
+    BEGIN
+        GET CURRENT DIAGNOSTICS CONDITION 1
+            @errno = MYSQL_ERRNO, @msg = MESSAGE_TEXT;
 
-    EXECUTE test_prep USING @person_id, @inactive, @vid ;
+        SET o_message = CONCAT(@errno, ': ', @msg);
 
-    SET person_id = @person_id;
-    SET inactive = @inactive;
+        SET o_error = TRUE;
+    END;
 
-    DEALLOCATE PREPARE test_prep;
+    PREPARE inactivate_person_role
+    FROM -- WARNING id is t_person_role.id NOT t_person_role.person_id.
+      'UPDATE t_person_role SET term_end_date = ?, inactive = TRUE WHERE id = ?'; 
 
+    SET @end_date = p_report_date;
+
+    SELECT id 
+      INTO @person_role_id
+      FROM t_person_role
+     WHERE person_id = p_person_id
+       AND role_id   = c_pcp_role_id
+       AND inactive  = c_inactive_false;
+    
+    GET CURRENT DIAGNOSTICS CONDITION 1
+    @errno = MYSQL_ERRNO, @msg = MESSAGE_TEXT;
+
+    IF @errno IS NULL
+    THEN
+        EXECUTE inactivate_person_role USING @end_date, @person_role_id;
+        SET o_error = FALSE;
+        SET o_message = NULL;
+    ELSE
+        SET o_error = TRUE;
+        SET o_message = CONCAT(@errno, ': ', @msg);
+    END IF;
 END //
 
 DELIMITER ;
