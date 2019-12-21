47f3f16071f575fa94da45954bcd7bc58f483a09
George Yolland <gyolland@yahoo.com>
2019-12-20 22:33:05 -0800

    added query to update ocvr_voter_id in member_stage

--- db/sqlscripts/insert_ocvr_fr_member_stage.sql
+++ db/sqlscripts/insert_ocvr_fr_member_stage.sql
@@ -1,6 +1,32 @@
 select * from t_import_ocvr_tmp;
 truncate t_import_ocvr_tmp;
 
+-- start: update member stage with exiting ocvr_voter_id from t_person
+SELECT
+    p.id
+  , p.ocvr_voter_id
+  , m.voter_id
+  , p.fname
+  , p.middlename
+  , p.lname
+  , m.fname AS mfname
+  , m.middlename AS mmiddlename
+  , m.lname AS mlname
+  FROM t_person p 
+  JOIN member_stage m ON p.id = m.member_id
+ WHERE p.ocvr_voter_id <> m.voter_id;
+
+set sql_safe_updates = 0;
+
+start transaction;
+update member_stage m, t_person p
+SET m.voter_id = p.ocvr_voter_id
+where m.member_id = p.id
+and m.voter_id <> p.ocvr_voter_id;
+
+commit;
+-- start: update member stage with exiting ocvr_voter_id from t_person
+
 insert into t_import_ocvr_tmp( precinct, ocvr_voter_id, lname, fname, r_address, r_city, r_state, r_zip, assignment)
 select 
     m.precinct
